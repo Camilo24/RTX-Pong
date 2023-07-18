f1fc99feac265253730781efaf27855073e5866a
camiloalejo2006@gmail.com <camiloalejo2006@gmail.com>
2023-07-18 13:59:09 -0500

    Comportamiento de la pelota

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private float _initialPositionX, _initialPositionZ;
     private Rigidbody _rigidBody;
-    private Vector3 _impulse;
-    private float _velocity = 5f;
+    private Vector3 _initialPosition;
+    private float _velocity = 50f;
+    //private float _velocityIncrease = 1.01f;
 
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
-
-        _initialPositionX = transform.position.x;
-        _initialPositionZ = transform.position.z;
+        _rigidBody.AddForce(new Vector3(1, 0, 1) * _velocity, ForceMode.VelocityChange);
     }
 
     void Update()
     {
-        _impulse = new Vector3(transform.position.x + _velocity, transform.position.y, transform.position.z + _velocity);
-        _rigidBody.AddForce(_impulse, ForceMode.Impulse);
+        _rigidBody.velocity = _rigidBody.velocity.normalized * _velocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Programar que al colisionar con cualquier cosa se cambie su direccion
+        Vector3 reflectDirection = Vector3.Reflect(_rigidBody.velocity.normalized, collision.contacts[0].normal);
+        //_velocity *= _velocityIncrease;
+        _rigidBody.velocity = reflectDirection * _velocity;
     }
 }
